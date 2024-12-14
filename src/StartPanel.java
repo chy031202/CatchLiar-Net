4d657b11d3701e3c52d7b6f642a0a6e8b665ff14
se0y <gong021200@naver.com>
2024-12-15 02:24:58 +0900

    FEAT : 시작 화면 엔터로 접속

--- src/StartPanel.java
+++ src/StartPanel.java
@@ -6,7 +6,6 @@ import java.io.IOException;
 
 public class StartPanel extends JPanel {
     private JTextField t_nickname;
-    private JButton b_start;
 
     private ClientManager clientManager;
 
@@ -45,32 +44,20 @@ public class StartPanel extends JPanel {
         JLabel nicknameLabel = new JLabel("닉네임");
         nicknameLabel.setAlignmentX(JLabel.CENTER_ALIGNMENT);
 
-        t_nickname = new JTextField(15);
+        t_nickname = new JTextField(10);
         t_nickname.setMaximumSize(t_nickname.getPreferredSize()); // 입력 필드의 크기를 고정
+        t_nickname.addActionListener(e -> connectAndSendNickname());
         t_nickname.setAlignmentX(JTextField.CENTER_ALIGNMENT);
 
         ImageIcon originalIcon = new ImageIcon(getClass().getResource("/images/start.png"));
         Image resizedImage = originalIcon.getImage().getScaledInstance(60, 60, Image.SCALE_SMOOTH);
         ImageIcon resizedIcon = new ImageIcon(resizedImage);
         JLabel startButton = new JLabel(resizedIcon);
-
         // MouseListener 추가
         startButton.addMouseListener(new java.awt.event.MouseAdapter() {
             @Override
             public void mouseClicked(java.awt.event.MouseEvent e) {
-                try {
-                    String nickname = t_nickname.getText();
-                    if (nickname.isEmpty()) {
-//                        JOptionPane.showMessageDialog(frame, "닉네임을 입력해주세요!", "경고", JOptionPane.WARNING_MESSAGE);
-                        return;
-                    }
-                    clientManager.connectToServer();
-                    clientManager.sendNickname(nickname); // 닉네임 서버에 전송
-
-                } catch (IOException ex) {
-                    System.err.println(ex.getMessage());
-                    System.exit(-1);
-                }
+                connectAndSendNickname();
             }
         });
         startButton.setAlignmentX(JTextField.CENTER_ALIGNMENT);
@@ -78,7 +65,7 @@ public class StartPanel extends JPanel {
         backgroundPanel.add(Box.createVerticalGlue()); // 컴포넌트를 수직으로 가운데 정렬
         backgroundPanel.add(Box.createVerticalStrut(180)); // 컴포넌트 간 여백 추가
         backgroundPanel.add(startButton);
-        backgroundPanel.add(Box.createVerticalStrut(100));
+        backgroundPanel.add(Box.createVerticalStrut(70));
         backgroundPanel.add(nicknameLabel);
         backgroundPanel.add(Box.createVerticalStrut(10));
         backgroundPanel.add(t_nickname);
@@ -87,5 +74,20 @@ public class StartPanel extends JPanel {
         // 배경 패널을 프레임에 추가
         add(backgroundPanel, BorderLayout.CENTER);
 
+    }
+
+    private void connectAndSendNickname() {
+        try {
+            String nickname = t_nickname.getText();
+            if (nickname.isEmpty()) {
+                // JOptionPane.showMessageDialog(this, "닉네임을 입력해주세요!", "경고", JOptionPane.WARNING_MESSAGE);
+                return;
+            }
+            clientManager.connectToServer();
+            clientManager.sendNickname(nickname); // 닉네임 서버에 전송
+        } catch (IOException ex) {
+            System.err.println(ex.getMessage());
+            System.exit(-1);
+        }
     }
 }
